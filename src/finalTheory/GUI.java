a20be0419f6295518343b12581303c53e4f8e2dd
Huỳnh Thịnh <minhthinhlss@gmail.com>
2020-01-06 20:47:18 +0700

    Update Command Pattern

--- src/finalTheory/GUI.java
+++ src/finalTheory/GUI.java
@@ -10,10 +10,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- *
- * @author Student
+ * Receiver.
  */
-
 /*public*/ class User {
 
     public User() {
@@ -31,11 +29,17 @@ import java.util.List;
     }
 }
 
+/**
+ * Command.
+ */
 /*public*/ interface IClick {
 
     void click();
 }
 
+/**
+ * Abstract Command.
+ */
 /*public*/ abstract class OnMenuItem implements IClick {
 
     protected List<User> users;
@@ -45,6 +49,9 @@ import java.util.List;
     }
 }
 
+/**
+ * Concrete Command.
+ */
 /*public*/ class OnMenuItem1 extends OnMenuItem {
 
     public OnMenuItem1(List _users) {
@@ -59,6 +66,9 @@ import java.util.List;
     }
 }
 
+/**
+ * Concrete Command.
+ */
 /*public*/ class OnMenuItem2 extends OnMenuItem {
 
     public OnMenuItem2(List _users) {
@@ -73,23 +83,46 @@ import java.util.List;
     }
 }
 
+/**
+ * INVOKER.
+ */
+/*public*/ class Menu {
+
+    private IClick menuItem;
+
+    public Menu() {
+
+    }
+
+    public void setMenuItem(IClick _menuItem) {
+        menuItem = _menuItem;
+    }
+
+    public void click() {
+        menuItem.click();
+    }
+}
+
+/**
+ * CLIENT.
+ */
 public class GUI {
 
     private List<User> users;
-    private IClick onMenuItem;
+    private Menu menu = new Menu();
 
     public GUI() {
         users = new ArrayList<User>();
     }
 
     public void onMenuItem1_Click() {
-        onMenuItem = new OnMenuItem1(users);
-        onMenuItem.click();
+        menu.setMenuItem(new OnMenuItem1(users));
+        menu.click();
     }
 
     public void onMenuItem2_Click() {
-        onMenuItem = new OnMenuItem2(users);
-        onMenuItem.click();
+        menu.setMenuItem(new OnMenuItem2(users));
+        menu.click();
     }
 
     public static void main(String args[]) {
